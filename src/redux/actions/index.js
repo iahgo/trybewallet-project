8b890a8d0f48f82996a69d060d47e61dcf6ce9f8
iahgo <iahgo@icloud.com>
2022-08-02 23:47:24 -0300

    finalmente req 1 OK

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,23 @@
 // Coloque aqui suas actions
+import * as actionsTypes from './actionsTypes';
+
+export const usuario = (payload) => ({
+  type: actionsTypes.SAVE_EMAIL,
+  payload,
+});
+
+export const setMoedas = (array) => ({
+  type: actionsTypes.GET_CURRENCIES,
+  payload: array,
+});
+
+export function getCurrencies() {
+  return (dispatch) => (
+    fetch('https://economia.awesomeapi.com.br/json/all')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        dispatch(setMoedas(data));
+      })
+  );
+}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,18 @@
 // configure aqui sua store
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk),
+  ),
+);
+
+if (window.Cypress) {
+  window.store = store;
+}
+
+export default store;
