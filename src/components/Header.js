e064dddea8fdf26f7f1412f18782f1519fdb8f6f
iahgo <iahgo@icloud.com>
2022-08-03 00:37:17 -0300

    requisito 2 OK

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <div id="header">
+        Header
+        <div data-testid="email-field">
+          Email:
+          {' '}
+          { email }
+        </div>
+        <div data-testid="total-field">
+          0
+        </div>
+        <div data-testid="header-currency-field">
+          BRL
+        </div>
+      </div>
     );
   }
 }
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
